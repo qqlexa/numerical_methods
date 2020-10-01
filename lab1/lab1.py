ad01bc4f0cdba440d179c8636b3c77bc269d4bc3
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-01 23:54:31 +0300

    lab1.py
    Added iteration method

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,54 +1,11 @@
-"""
-https://www.wolframalpha.com/input/?i=%28x+-+42%29+%5E+2+%2B+sin%28x+-+42%29
+# https://www.wolframalpha.com/input/?i=%28x+-+42%29+%5E+2+%2B+sin%28x+-+42%29
 
-Лабораторна робота 1
-
-Тема: Чисельне розв’язання нелінійних рівнянь
-Дисципліна: ЧИСЕЛЬНІ МЕТОДИ
-
-- Розробити алгоритми та програмне забезпечення для розв’язку наведеної задачі вказаними методами.
-- Алгоритми представити у вигляді блок-схем або діаграм діяльності UML.
-+ Програмне забезпечення розробити на будь-якій сучасній мові програмування.
-- Відділеня коренів здійснити побудовою графіків функції з використанням математичних пакетів.
-- Абсолютна похибка має дорівнювати 0,01.
-- Знайти точне значення розв’язку задачі за допомогою математичних бібліотек та порівняти його зі значенням, отриманим
-у результаті роботи розробленого програмного забезпечення.
-
-+ 1.Знайти розв’язок рівняння  f(x) = (x - g * k)^2 + sin(x- g * k) = 0
-(де g – остання цифра у номері студентського квитка, а k – передостання)
-методами ділення +навпіл+, +хорд+ та +дотичних+ (Ньютона).
-
-2.Знайти розв’язок рівняння f(x) = (k * x - 10g) - sin(x - 10g / k) = 0
-(де g – остання цифра у номері студентського квитка, а k – передостання)
-методом ітерацій. < < < < <
-
-
-Звіт з завдання має містити:
-1.	Титульний аркуш.
-2.	Завдання відповідно до варіанту.
-3.	Опис розв’язку задачі (математичну постановку, алгоритми, лістинг програми, результати її роботи,
-пошук точного рішення, висновки на основі порівняння точного розв’язку та знайденого за допомогою розробленої програми).
-Захист звіту передбачає також опитування з теоретичних питань щодо застосованих методів та засобів.
-Максимальна оцінка за роботу – 10 балів.
-При несвоєчасному захисті звіту штраф 5 балів.
-
-"""
-from sympy import symbols, sin, diff, solve
+from sympy import sin
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-
-
-def find_diff():
-    x = symbols("x")
-    eq_1 = diff((k * x - 10 * g) - sin(x - 10 * g / k))
-    eq_2 = diff(diff((k * x - 10 * g) - sin(x - 10 * g / k)))
-
-    print(eq_1)
-    print(eq_2)
-
-    print(eq_1.subs(x, 42))
-    print(eq_2.subs(x, 42))
+from scipy.optimize import fsolve
+from colorama import Fore
 
 
 def count_f1(x):
@@ -60,23 +17,30 @@ def d_f1(x):
 
 
 def dd_f1(x):
-    return 2 - sin(x - 42)
+    return 2 - math.sin(x - 42)
 
 
-def count_f2(x):
-    return (k * x - 10 * g) - math.sin(x - 10 * g / k)
+def count_phi2(x):
+    """
+    (kx - 10g) - sin(x - 10g/k) = 0
+    kx = sin(x - 10g/k) + 10g
+    x = (sin(x - 10g/k) + 10g)/k
+    """
+    return (math.sin(x - 10 * g / k) + 10 * g) / k
 
 
-def d_f2(x):
-    return 6 - math.cos(x - 11.6666666666667)
+def create_plot(x, y, parameters):
+    """
 
-
-def dd_f2(x):
-    return math.sin(x - 11.6666666666667)
-
-
-def create_plot(x, y):
-    print("Plot")
+    :param x: argument
+    :param y: function
+    :param parameters: list from [dict(), dict(), dict() .. . . . .]
+    dict:
+        "color"
+        "description"
+        "solution"
+    :return:
+    """
     # setting the axes at the centre
     fig = plt.figure()
 
@@ -89,10 +53,8 @@ def create_plot(x, y):
     ax.yaxis.set_ticks_position('left')
 
     # plot the function
-    for i in y:
-        plt.plot(x, i[0], i[1], label=i[2])
-        for j in i[3]:
-            plt.plot(x, i[0], i[1], label="Solution: %s " % j)
+    for i in parameters:
+        plt.plot(x, y, i["color"], label="{method} {solution}".format(method=i["description"], solution=i["solution"]))
 
     # show the plot
     plt.legend(loc="upper left")
@@ -101,61 +63,59 @@ def create_plot(x, y):
 
 
 def find_result_by_half(a, b, d, eps, f):
-    solution = []
+    solutions = []
     for i in ((a, b), (b, d)):
         a = i[0]
         b = i[1]
-        if f(a) * f(b) < 0:
+        while b - a > eps:
+            c = (b + a) / 2
 
-            while b - a > eps:
-                c = (b + a) / 2
+            if abs(f(c)) <= eps ** 2:
+                break
 
-                if 0 <= abs(f(c)) <= eps:
-                    break
+            elif f(a) * f(c) < 0:
+                # [a, c]
+                b = c
 
-                elif f(a) * f(c) < 0:
-                    # [a, c]
-                    b = c
+            elif f(c) * f(b) < 0:
+                # [c, b]
+                a = c
+            else:
+                print("solution is not found")
+                break
 
-                elif f(c) * f(b) < 0:
-                    # [c, b]
-                    a = c
-                else:
-                    print("Solution is not found")
+        solutions.append((b + a) / 2)
 
-            solution.append((b + a) / 2)
-
-    return solution
+    return solutions
 
 
 def find_result_by_chord(a, b, d, eps, f):
-    solution = []
+    solutions = []
     for i in ((a, b), (b, d)):
 
         a = i[0]
         b = i[1]
-        if f(a) * f(b) < 0:
+        x_t = a - ((f(a) * (b - a)) / (f(b) - f(a)))
+        x_p = a
+        while abs(x_t - x_p) > eps:
+            if abs(f(x_t)) <= eps ** 2:
+                break
+            elif f(a) * f(x_t) < 0:
+                b = x_t
+            elif f(x_t) * f(b) < 0:
+                a = x_t
+            else:
+                print("solutions not found")
+                break
+            x_p = x_t
             x_t = a - ((f(a) * (b - a)) / (f(b) - f(a)))
-            x_p = a
-            while abs(x_t - x_p) > eps:
-                if abs(f(x_t)) <= eps ** 2:
-                    break
-                elif f(a) * f(x_t) < 0:
-                    b = x_t
-                elif f(x_t) * f(b) < 0:
-                    a = x_t
-                else:
-                    print("Solution not found")
-                    break
-                x_p = x_t
-                x_t = a - ((f(a) * (b - a)) / (f(b) - f(a)))
-            solution.append(x_t)
+        solutions.append(x_t)
 
-    return solution
+    return solutions
 
 
 def find_result_by_tangent(a, b, d, eps, f, f_d, f_dd):
-    solution = []
+    solutions = []
     for i in ((a, b), (b, d)):
         a = i[0]
         b = i[1]
@@ -166,56 +126,99 @@ def find_result_by_tangent(a, b, d, eps, f, f_d, f_dd):
             # f(b) * f_dd(b) > 0
             x_t = b
 
-        x_p = eps * 2
+        x_p = b if x_t == a else a
 
         while abs(x_t - x_p) > eps:
             x_p = x_t
             x_t = x_p - f(x_p) / f_d(x_p)
 
-        solution.append(x_t)
+        solutions.append(x_t)
 
-    return solution
+    return solutions
+
+
+def find_result_by_iterations(a, b, eps, f):
+    solutions = []
+    x_p = (a + b) / 2
+    x_t = f(x_p)
+    while abs(x_t - x_p) > eps:
+        x_p = x_t
+        x_t = f(x_p)
+
+    solutions.append(x_t)
+
+    return solutions
 
 
 def main():
-    global k, g
+    global g, k
     eps = 0.01
     student_id = "12648067"
     g = int(student_id[-1])  # 7
     k = int(student_id[-2])  # 6
 
-    a = 40
+    a = 40.0
     b = 41.7
     d = 42.5
 
     print("g = {g} | k = {k} ".format(g=g, k=k))
 
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fsolve.html#:~:text=Find%20the%20roots%20of%20a,0%20given%20a%20starting%20estimate.&text=A%20function%20that%20takes%20at,value%20of%20the%20same%20length
+
+    def func(m):
+        return (m - g * k) ** 2 + np.sin(m - g * k)
+
+    def func2(m):
+        return (k * m - 10 * g) - np.sin(m - 10 * g / k)
+
+    root = fsolve(func, [a, d])
+    print("(x - {g} * {k}) ** 2 + sin(x - {g} * {k})".format(g=g, k=k))
+    print("The right solve:" + Fore.GREEN)
+    print(root)
+
     # half-method
+    print(Fore.RESET + "half-method solutions")
     print(find_result_by_half(a, b, d, eps, count_f1))
-    # print(find_result_by_half(a, b, d, eps, count_f2))
 
     # chord-method
-    print("chord-method")
+    print("chord-method solutions")
     print(find_result_by_chord(a, b, d, eps, count_f1))
-    # print(find_result_by_chord(a, b, d, eps, count_f2))
 
-    print()
-    print("tangent-method")
     # tangent-method
+    print("tangent-method solutions")
     print(find_result_by_tangent(a, b, d, eps, count_f1, d_f1, dd_f1))
-    # print(find_result_by_tangent(a, b, d, eps, count_f2, d_f2, dd_f2))
 
     # draw plot
     x = np.linspace(-500, 500, 1000000)
 
-    y1 = (x - g * k) ** 2 + np.sin(x - g * k)
-    # y2 = (k * x - 10 * g) - np.sin(x - 10 * g / k)
-    create_plot(x, [[y1, "r", "(x - {g} * {k}) ** 2 + sin(x - {g} * {k})".format(k=k, g=g),
-                     find_result_by_half(a, b, d, eps, count_f1)],
-                    [y1, "g", "(x - {g} * {k}) ** 2 + sin(x - {g} * {k})".format(k=k, g=g),
-                     find_result_by_chord(a, b, d, eps, count_f1)],
-                    [y1, "b", "(x - {g} * {k}) ** 2 + sin(x - {g} * {k})".format(k=k, g=g),
-                     find_result_by_tangent(a, b, d, eps, count_f1, d_f1, dd_f1)]])
+    y = (x - g * k) ** 2 + np.sin(x - g * k)
+
+    create_plot(x, y, [{"color": "r", "description": "Half: ",
+                        "solution": find_result_by_half(a, b, d, eps, count_f1)},
+                       {"color": "g", "description": "Chord: ",
+                        "solution": find_result_by_chord(a, b, d, eps, count_f1)},
+                       {"color": "b", "description": "Tangent: ",
+                        "solution": find_result_by_tangent(a, b, d, eps, count_f1, d_f1, dd_f1)}])
+
+    print()
+    print()
+    
+    y = (k * x - 10 * g) - np.sin(x - 10 * g / k)
+
+    a = 11.5
+    b = 11.7
+
+    root = fsolve(func2, [a, b])
+    print("({k} * x - 10 * {g}) - sin(x - 10 * {g} / {k})".format(g=g, k=k))
+    print("The right solve:" + Fore.GREEN)
+    print(root)
+
+    # iterations-method
+    print(Fore.RESET + "iterations-method solution")
+    print(find_result_by_iterations(a, b, eps, count_phi2))
+
+    create_plot(x, y, [{"color": "b", "description": "Iterations: ",
+                       "solution": find_result_by_iterations(a, b, eps, count_phi2)}])
 
 
 if __name__ == '__main__':
