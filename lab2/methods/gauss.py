3644790f4e0e0703a94472880b3f2bb8ef20113d
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-21 21:10:19 +0300

    Add files via upload

--- lab2/methods/gauss.py
+++ lab2/methods/gauss.py
@@ -1,2 +1,16 @@
 # Gauss matrix method
 
+def solve(a, b):
+    # A - matrix
+    # B -
+    # X - array of solutions
+    x = list()
+    for i, j in zip(a, b):
+        for i2 in i:
+            print(i2, end=" ")
+        print(j)
+
+    return x
+
+
+solve([[4, 5], [4, 6]], [2, 3])
