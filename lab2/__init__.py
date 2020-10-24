8658e38211b4a67daab9224dfc68c7f69a913ac1
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-24 13:15:24 +0300

    Add files via upload

---
+++ lab2/__init__.py
@@ -0,0 +1,25 @@
+__title__ = 'matrix_solver'
+__author__ = 'qqlexa'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 qqlexa'
+__version__ = '0.0.1'
+
+
+def get_title():
+    return __title__
+
+
+def get_author():
+    return __author__
+
+
+def get_license():
+    return __license__
+
+
+def get_copyright():
+    return __copyright__
+
+
+def get_version():
+    return __version__
