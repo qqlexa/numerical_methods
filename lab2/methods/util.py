e60f64ec0bbc911a447c015586880f02f316ac2f
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-24 12:09:39 +0300

    Add files via upload

--- lab2/methods/util.py
+++ lab2/methods/util.py
@@ -6,11 +6,10 @@ def print_equation(a, b):
     print()
 
 
-def create_matrix(n):
-    c = list()
-    for _ in range(n):
-        x = list()
-        for j in range(n):
-            x.append(j)
-        c.append(x)
-    return c
+def print_equation_with_c(a, c, b):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(a[i][c[j]], end=" ")
+        print(b[i])
+    print()
+
