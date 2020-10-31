d9dfbe559ecf5333688476f0f0fa72b8c28d3cbf
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-31 13:59:59 +0200

    Update gauss.py

--- lab2/methods/gauss.py
+++ lab2/methods/gauss.py
@@ -16,20 +16,21 @@ def count_array(a, b, i, i2):
             number = float(a[i2][i] / a[i][i])
             matrix.mul_row(a, i, number)
             b[i] *= number
-            util.print_equation(a, b)
+            # util.print_equation(a, b)
             matrix.row_minus_row(a, from_i=i2, minus_i=i)
             b[i2] -= b[i]
         else:
-            print(f"a[{i2}][{i}] == 0")
+            # print(f"a[{i2}][{i}] == 0")
+            pass
     else:
-        print(f"a[{i}][{i}] == 0")
+        # print(f"a[{i}][{i}] == 0")
+        pass
 
 
 def solve(a, b):
     """
     :param a: a: matrix
     :param b: b: matrix (additional)
-              c: vector location of columns
     :return:  x: vector of solutions
     """
     n = len(a)
@@ -47,12 +48,12 @@ def solve(a, b):
     for i in range(n):
         for i2 in range(i + 1, n):
             count_array(a, b, i, i2)
-        util.print_equation(a, b)
+        # util.print_equation(a, b)
 
     for i in range(n - 1, -1, -1):
         for i2 in range(i - 1, -1, -1):
             count_array(a, b, i, i2)
-        util.print_equation(a, b)
+        # util.print_equation(a, b)
 
     for i in range(n):
         if a[i][i] != 0:
