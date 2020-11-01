3e2da746500ef2102a254a5fa4884b6f6c074df1
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-01 12:52:40 +0200

    Add files via upload

---
+++ lab2/src/main/python/test_is_square_matrix.py
@@ -0,0 +1,26 @@
+import unittest
+from methods import matrix
+
+
+class TestDeterminant(unittest.TestCase):
+    def test_square_1(self):
+        self.assertIs(matrix.is_square_matrix([1]), False, "Should be False")
+
+    def test_square_11(self):
+        self.assertIs(matrix.is_square_matrix([[1]]), True, "Should be True")
+
+    def test_square_12(self):
+        self.assertIs(matrix.is_square_matrix([1, 2]), False, "Should be False")
+
+    def test_square_21(self):
+        self.assertIs(matrix.is_square_matrix([[1], [2]]), False, "Should be False")
+
+    def test_square_22(self):
+        self.assertIs(matrix.is_square_matrix([[1, 2], [3, 4]]), True, "Should be True")
+
+    def test_square_22_tuple(self):
+        self.assertIs(matrix.is_square_matrix(((1, 2), (3, 4))), True, "Should be True")
+
+
+if __name__ == '__main__':
+    unittest.main()
