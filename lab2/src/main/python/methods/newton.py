e350ef234f19b635714b1cdf80a0cad4f40dc1c2
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-12 22:04:10 +0200

    Add files via upload

--- lab2/src/main/python/methods/newton.py
+++ lab2/src/main/python/methods/newton.py
@@ -1,2 +1,19 @@
 # Newton method
+import math
 
+try:
+    import util
+    import matrix
+except:
+    try:
+        from . import util
+        from . import matrix
+    except:
+        pass
+
+
+
+def solve():
+    pass
+
+solve()
